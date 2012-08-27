173fa4b31725667ac45d30e85df5b0065f5479aa
June Jung <jeongjueun@gmail.com>
2012-08-26 19:44:06 -0700

    reorganized some of the board.rb tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,41 +3,43 @@ require './board.rb'
 
 describe 'Board' do 
 
-	context 'initialize' do
-		it 'should raise error when argument is not a String' do
-			expect{
-				Board.new({:width => 5, :height => 5})
-			}.to raise_error
+	context "initializing" do
+		context 'when it is initialized with a String' do
+			it 'should raise error when argument is not a String' do
+				expect{
+					Board.new({:width => 5, :height => 5})
+				}.to raise_error
+			end
+			it 'should take a String as an argument' do
+				expect{
+					Board.new("5 5")
+				}.to_not raise_error
+			end
 		end
-		it 'should take a String as an argument' do
-			expect{
-				Board.new("5 5")
-			}.to_not raise_error
-		end
-	end
 
-	context 'when it initialized with a String' do
+		context 'creating a board' do
 
-		before(:each) do
-			@mars_rover = Board.new("10 4")
-		end
-		it 'should set first number argument as horizontal integer' do
-			@mars_rover.horizontal.should == 10
-		end
-		it 'should set first number argument as vertical integer' do
-			@mars_rover.vertical.should == 4
-		end
-		it 'should create board array' do
-			@mars_rover.board.should be_an_instance_of Array
-		end
-		it 'should have horizontal numebr of arrays' do
-			@mars_rover.board.length.should equal @mars_rover.horizontal
-		end
-		it 'should have vertical numebr of arrays' do
-			@mars_rover.board[0].length.should equal @mars_rover.vertical
-		end
-		it 'should have same numebr of arrays in board' do
-			@mars_rover.board[5].length.should equal @mars_rover.vertical
+			before(:each) do
+				@mars_rover = Board.new("10 4")
+			end
+			it 'should set first number argument as horizontal integer' do
+				@mars_rover.horizontal.should == 10
+			end
+			it 'should set first number argument as vertical integer' do
+				@mars_rover.vertical.should == 4
+			end
+			it 'should create board array' do
+				@mars_rover.board.should be_an_instance_of Array
+			end
+			it 'should have horizontal numebr of arrays' do
+				@mars_rover.board.length.should equal @mars_rover.horizontal
+			end
+			it 'should have vertical numebr of arrays' do
+				@mars_rover.board[0].length.should equal @mars_rover.vertical
+			end
+			it 'should have same numebr of arrays in board' do
+				@mars_rover.board[5].length.should equal @mars_rover.vertical
+			end
 		end
 	end
 end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -3,7 +3,7 @@ require './mars_rover.rb'
 
 describe 'Mars_rover' do 
 
-	context 'initialize' do
+	context 'when it initialized with an existing file' do
 		it 'should raise error when file can not be found' do
 			expect{
 				Mars_rover.new('./spec/absent.txt')
@@ -16,25 +16,25 @@ describe 'Mars_rover' do
 		end
 	end
 
-	# context 'when it initialized with a String' do
+	context 'dsf' do
 
-	# 	before(:each) do
-	# 		@mars_rover = Board.new("10 4")
-	# 	end
-	# 	it 'should set first number argument as horizontal integer' do
-	# 		@mars_rover.horizontal.should == 10
-	# 	end
-	# 	it 'should set first number argument as vertical integer' do
-	# 		@mars_rover.vertical.should == 4
-	# 	end
-	# 	it 'should create board array' do
-	# 		@mars_rover.board.should be_an_instance_of Array
-	# 	end
-	# 	it 'should have horizontal numebr of arrays' do
-	# 		@mars_rover.board.length.should equal @mars_rover.horizontal
-	# 	end
-	# 	it 'should have vertical numebr of arrays' do
-	# 		@mars_rover.board[0].length.should equal @mars_rover.vertical
-	# 	end
-	# end
+		before(:each) do
+			@mars_rover = Board.new("10 4")
+		end
+		# it 'should set first number argument as horizontal integer' do
+		# 	@mars_rover.horizontal.should == 10
+		# end
+		# it 'should set first number argument as vertical integer' do
+		# 	@mars_rover.vertical.should == 4
+		# end
+		# it 'should create board array' do
+		# 	@mars_rover.board.should be_an_instance_of Array
+		# end
+		# it 'should have horizontal numebr of arrays' do
+		# 	@mars_rover.board.length.should equal @mars_rover.horizontal
+		# end
+		# it 'should have vertical numebr of arrays' do
+		# 	@mars_rover.board[0].length.should equal @mars_rover.vertical
+		# end
+	end
 end
