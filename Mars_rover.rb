db0aa0b0ae4fcb9a2e8c4588ad5438811b997c31
June Jung <jeongjueun@gmail.com>
2012-08-26 19:53:15 -0700

    set up Gemfile, readme, rspec and rvmrc.

--- Mars_rover.rb
+++ Mars_rover.rb
@@ -1,15 +1,14 @@
 class Mars_rover
-	attr_accessor :document, :f
 
 	def initialize(filename)
-		unless File.exists? "#{filename}"
+		unless File.exists? "./#{filename}"
 			raise "Can't find file."
 		end
 		@document = []
-		@f = File.readlines("#{filename}")
-		@f.each { |l| @document << l }
+		f = File.readlines("./#{filename}")
+		f.each { |l| @document << l }
 	end
+
 end
 
 m = Mars_rover.new('input.txt')
-puts m.document.inspect
