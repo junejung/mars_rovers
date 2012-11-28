8a698c8052c610879df9c11134d8e70a92228c75
Catalyst Students <accounts@catalystclass.com>
2012-11-27 22:27:41 -0800

    clean up controller

--- run_me.rb
+++ run_me.rb
@@ -1,6 +1,27 @@
 require './controller.rb'
 
 file_name = ARGV[0] || 'input.txt'
-my_mars_rover = Controller.new(file_name)
+file_name = "./#{file_name}"
+unless File.exists? file_name
+	raise "Can't find file."
+end
+@document = File.readlines(file_name)
+@document.each { |e| 
+	e.gsub("\n","")  
+}
+@board = Board.new(@document.shift)
+
+until @document.empty?
+	rover = Rover.new(@document.shift, @document.shift)
+	@board.save_final_position(rover)
+end
+
+
+
 puts "Printing data for #{file_name}:"
-my_mars_rover.output
+
+output_string = ''
+@board.present_entities.each do |entity|
+	output_string += entity.join(' ') + "\n"
+end
+puts output_string
